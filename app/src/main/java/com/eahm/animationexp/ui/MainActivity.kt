d6780535287d97fdbe4c0b29a2c95153835d1662
elvishrdz <elvishrdz@gmail.com>
2020-08-22 14:50:42 +0200

    Initial commit

---
+++ app/src/main/java/com/eahm/animationexp/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.eahm.animationexp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.eahm.animationexp.R
+import com.eahm.animationexp.adapters.MainAdapter
+import com.eahm.animationexp.models.MenuElement
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val appSections = listOf(
+            MenuElement(
+                    "The app menu is ready!",
+                    "Goes to MainActivity",
+                    "This is a basic example of the app menu for each new activity where a new functionality will be tested.",
+                    MainActivity::class.java
+            )
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvMainSections.apply {
+            adapter = MainAdapter(appSections.asReversed())
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
+        }
+    }
+}
